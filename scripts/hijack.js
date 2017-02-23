99a27a71a966ccaff3c67bbce9423d31d3efc272
yaserig <mr.yaserig@gmail.com>
2017-02-22 17:46:43 -0800

    done with ch 6

---
+++ scripts/hijack.js
@@ -0,0 +1,11 @@
+var linkArray =[].slice.call(document.querySelectorAll('a'));
+
+function disableClick(link) {
+    'use strict';
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+		    console.log('You cannot click!!!')
+    });
+}
+
+linkArray.forEach(disableClick);
